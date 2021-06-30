ad3fc43aeb8d24b141f7594eab3fd15924cec9b5
dave-cmd <56073515+dave-cmd@users.noreply.github.com>
2021-06-30 12:00:15 -0400

    Update base_model.py
    __init__() method modified

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,23 @@ class BaseModel(object):
 	id = None
 	created_at = None
 	updated_at = None
-	def __init__(self):
-		self.my_number = ""
-		self.name = ""
-		self.id = uuid.uuid4()
-		self.created_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+	def __init__(self, *args, **kwargs):
+		if len(kwargs) > 0:
+			if "id" in kwargs:
+				self.id = kwargs["id"]
+			elif "my_number" in kwargs:
+				self.my_number = kwargs["my_number"]
+			elif "name" in kwargs:
+				self.name = kwargs["name"]
+			elif "created_at" in kwargs:
+				self.created_at = kwargs["created_at"]
+			elif "updated_at" in kwargs:
+				self.updated_at = kwargs["updated_at"]
+		else:
+			self.my_number = ""
+			self.name = ""
+			self.id = uuid.uuid4()
+			self.created_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
 
 	def to_dict_(self):
 		return dict((key, value) for key, value in BaseModel.__dict__.items() if key[:2] != "__" )
