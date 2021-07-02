f95ad4ebca82ba1afdf729359ef16c0696a8d3c3
Kanjuru <kanjurus8@gmail.com>
2021-07-02 19:05:39 +0000

    models complete

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,134 @@
-import uuid
-import datetime
-from models import storage
+#!/usr/bin/python3
+"""
+BaseModel Class of Models Module
+"""
 
-class BaseModel(object):
-	"""docstring for BaseModel"""
-	id = None
-	created_at = None
-	updated_at = None
-	def __init__(self, *args, **kwargs):
-		if len(kwargs) > 0:
-			if "id" in kwargs:
-				self.id = kwargs["id"]
-			elif "my_number" in kwargs:
-				self.my_number = kwargs["my_number"]
-			elif "name" in kwargs:
-				self.name = kwargs["name"]
-			elif "created_at" in kwargs:
-				self.created_at = kwargs["created_at"]
-			elif "updated_at" in kwargs:
-				self.updated_at = kwargs["updated_at"]
+import os
+import json
+import models
+from uuid import uuid4, UUID
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime
 
-		else:
-			self.my_number = ""
-			self.name = ""
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
-	def to_dict_(self):
-		return dict((key, value) for key, value in BaseModel.__dict__.items() if key[:2] != "__" )
-
-	def to_dict(self):
-		self.__dict__["__class__"] = self.__class__.__name__
-		return self.__dict__
-
-	def save(self):
-		self.updated_at = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
-		storage.save()
-		storage.new(self)
-
-	def __str__(self):
-		return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
+"""
+    Creates instance of Base if storage type is a database
+    If not database storage, uses class Base
+"""
+if STORAGE_TYPE == 'db':
+    Base = declarative_base()
+else:
+    class Base:
+        pass
 
 
-		
+class BaseModel:
+    """
+        attributes and functions for BaseModel class
+    """
 
+    if STORAGE_TYPE == 'db':
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
 
+    def __init__(self, *args, **kwargs):
+        """
+            instantiation of new BaseModel Class
+        """
+        if kwargs:
+            self.__set_attributes(kwargs)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
 
+    def __set_attributes(self, attr_dict):
+        """
+            private: converts attr_dict values to python class attributes
+        """
+        if 'id' not in attr_dict:
+            attr_dict['id'] = str(uuid4())
+        if 'created_at' not in attr_dict:
+            attr_dict['created_at'] = datetime.utcnow()
+        elif not isinstance(attr_dict['created_at'], datetime):
+            attr_dict['created_at'] = datetime.strptime(
+                attr_dict['created_at'], "%Y-%m-%d %H:%M:%S.%f"
+            )
+        if 'updated_at' not in attr_dict:
+            attr_dict['updated_at'] = datetime.utcnow()
+        elif not isinstance(attr_dict['updated_at'], datetime):
+            attr_dict['updated_at'] = datetime.strptime(
+                attr_dict['updated_at'], "%Y-%m-%d %H:%M:%S.%f"
+            )
+        if STORAGE_TYPE != 'db':
+            attr_dict.pop('__class__', None)
+        for attr, val in attr_dict.items():
+            setattr(self, attr, val)
 
+    def __is_serializable(self, obj_v):
+        """
+            private: checks if object is serializable
+        """
+        try:
+            obj_to_str = json.dumps(obj_v)
+            return obj_to_str is not None and isinstance(obj_to_str, str)
+        except:
+            return False
 
+    def bm_update(self, attr_dict=None):
+        """
+            updates the basemodel and sets the correct attributes
+        """
+        IGNORE = [
+            'id', 'created_at', 'updated_at', 'email',
+            'state_id', 'user_id', 'city_id', 'place_id'
+        ]
+        if attr_dict:
+            updated_dict = {
+                k: v for k, v in attr_dict.items() if k not in IGNORE
+            }
+            for key, value in updated_dict.items():
+                setattr(self, key, value)
+            self.save()
+
+    def save(self):
+        """
+            updates attribute updated_at to current time
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_json(self, saving_file_storage=False):
+        """
+            returns json representation of self
+        """
+        obj_class = self.__class__.__name__
+        bm_dict = {
+            k: v if self.__is_serializable(v) else str(v)
+            for k, v in self.__dict__.items()
+        }
+        bm_dict.pop('_sa_instance_state', None)
+        bm_dict.update({
+            '__class__': obj_class
+            })
+        if not saving_file_storage and obj_class == 'User':
+            bm_dict.pop('password', None)
+        return(bm_dict)
+
+    def __str__(self):
+        """
+            returns string type representation of object instance
+        """
+        class_name = type(self).__name__
+        return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
+
+    def delete(self):
+        """
+            deletes current instance from storage
+        """
+        models.storage.delete(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,123 @@
-import json 
-import os
+#!/usr/bin/python3
+"""
+Handles I/O, writing and reading, of JSON for storage of all class instances
+"""
+import json
+from models import base_model, amenity, city, place, review, state, user
+from datetime import datetime
+
+strptime = datetime.strptime
+to_json = base_model.BaseModel.to_json
 
 
-class FileStorage(object):
-	"""docstring for FileStorage"""
-	__file_path = "file.json"
-	__objects = {}
-	def __init__(self):
-		super(FileStorage, self).__init__()
+class FileStorage:
+    """
+        handles long term storage of all class instances
+    """
+    CNC = {
+        'BaseModel': base_model.BaseModel,
+        'Amenity': amenity.Amenity,
+        'City': city.City,
+        'Place': place.Place,
+        'Review': review.Review,
+        'State': state.State,
+        'User': user.User
+    }
+    """CNC - this variable is a dictionary with:
+    keys: Class Names
+    values: Class type (used for instantiation)
+    """
+    __file_path = './dev/file.json'
+    __objects = {}
 
-	def all(self):
-		#return dictionary __objects
-		return self.__objects
+    def all(self, cls=None):
+        """
+            returns private attribute: __objects
+        """
+        if cls is not None:
+            new_objs = {}
+            for clsid, obj in FileStorage.__objects.items():
+                if type(obj).__name__ == cls:
+                    new_objs[clsid] = obj
+            return new_objs
+        else:
+            return FileStorage.__objects
 
-	def new(self, obj):
-		# stores objects in __objects
-		self.__objects[f"{obj.__class__.__name__}" + "." + str(obj.id)] = obj.__dict__
+    def new(self, obj):
+        """
+            sets / updates in __objects the obj with key <obj class name>.id
+        """
+        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[bm_id] = obj
 
-	def save(self):
-		#serilize __objects to json file path
-		with open(self.__file_path, "r+") as file:
-			for item in self.__objects.items():
-				str_ = "{ "
-				str_ = str_+str(item[0]) + " : "
-				str_ = str_+json.dumps(item[1])
-				str_+" }\n"
-				file.write(str_)
-			file.close()
-			
-	def reload(self):
-		# deserialize json file to __objects
-		if self.__file_path:
-			with open(self.__file_path, 'w+') as file:
-				for item in file.read():
-					#self.__objects[item[0]] = json.loads(item[1])
-					obj = json.loads(item)
-					self.__objects[item.keys()[0]] = item[item.keys()[0]]
-				file.close()
+    def save(self):
+        """
+            serializes __objects to the JSON file (path: __file_path)
+        """
+        fname = FileStorage.__file_path
+        storage_d = {}
+        for bm_id, bm_obj in FileStorage.__objects.items():
+            storage_d[bm_id] = bm_obj.to_json(saving_file_storage=True)
+        with open(fname, mode='w', encoding='utf-8') as f_io:
+            json.dump(storage_d, f_io)
 
+    def reload(self):
+        """
+            if file exists, deserializes JSON file to __objects, else nothing
+        """
+        fname = FileStorage.__file_path
+        FileStorage.__objects = {}
+        try:
+            with open(fname, mode='r', encoding='utf-8') as f_io:
+                new_objs = json.load(f_io)
+        except:
+            return
+        for o_id, d in new_objs.items():
+            k_cls = d['__class__']
+            FileStorage.__objects[o_id] = FileStorage.CNC[k_cls](**d)
+
+    def delete(self, obj=None):
+        """
+            deletes obj from __objects if it's inside
+        """
+        if obj:
+            obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
+            all_class_objs = self.all(obj.__class__.__name__)
+            if all_class_objs.get(obj_ref):
+                del FileStorage.__objects[obj_ref]
+            self.save()
+
+    def delete_all(self):
+        """
+            deletes all stored objects, for testing purposes
+        """
+        try:
+            with open(FileStorage.__file_path, mode='w') as f_io:
+                pass
+        except:
+            pass
+        del FileStorage.__objects
+        FileStorage.__objects = {}
+        self.save()
+
+    def close(self):
+        """
+            calls the reload() method for deserialization from JSON to objects
+        """
+        self.reload()
+
+    def get(self, cls, id):
+        """
+            retrieves one object based on class name and id
+        """
+        if cls and id:
+            fetch_obj = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch_obj)
+        return None
+
+    def count(self, cls=None):
+        """
+        count of all objects in storage
+        """
+        return (len(self.all(cls)))
